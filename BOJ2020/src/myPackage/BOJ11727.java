42ab0db8322a5b9f54b3d99d1457aadc3bc22635
Hye-Yeon Choi <chlgpdus921@naver.com>
2020-03-07 20:50:25 +0900

    UPDATE BOJ11727

---
+++ BOJ2020/src/myPackage/BOJ11727.java
@@ -0,0 +1,25 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+/**
+ * 
+ * @FileName BOJ11727.java
+ * @author chlgpdus921
+ * @date 2020. 3. 7.
+ */
+public class BOJ11727 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.valueOf(br.readLine());
+		int[] d = new int[n + 1];
+		d[0] = 1;
+		d[1] = 1;
+
+		for (int i = 2; i <= n; i++) {
+			d[i] = (d[i - 1] + 2 * d[i - 2]) % 10007;
+		}
+		System.out.println(d[n]);
+	}
+}
