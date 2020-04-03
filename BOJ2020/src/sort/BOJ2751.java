5a928e5a665992900d5028f6776a86071a97c4a2
Hye-Yeon Choi <chlgpdus921@naver.com>
2020-04-03 23:22:14 +0900

    UPDATE BOJ5751

--- BOJ2020/src/sort/BOJ2751.java
+++ BOJ2020/src/sort/BOJ2751.java
@@ -3,28 +3,60 @@ package sort;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 /**
  * 
  * @FileName BOJ2751.java
  * @author chlgpdus921
- * @date 2020. 3. 20.
+ * @date 2020. 4. 2.
  */
 /* 수 정렬하기 2 */
 public class BOJ2751 {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+
 		int n = Integer.valueOf(br.readLine());
-		int[] data = new int[n];
+		int[] arr = new int[n];
+
 		for (int i = 0; i < n; i++) {
-			data[i] = Integer.valueOf(br.readLine());
+			arr[i] = Integer.valueOf(br.readLine());
 		}
-		Arrays.sort(data);
+		quickSort(arr, 0, n - 1);
+
 		for (int i = 0; i < n; i++) {
-			sb.append(data[i] + "\n");
+			sb.append(arr[i] + "\n");
 		}
 		System.out.println(sb);
+
+	}
+
+	public static void quickSort(int[] arr, int left, int right) {
+		int pivot = arr[(left + right) / 2];
+		int l = left;
+		int r = right;
+
+		while (l <= r) {
+			while (arr[l] < pivot)
+				l++;
+
+			while (arr[r] > pivot)
+				r--;
+
+			if (l <= r) {
+				int temp = arr[l];
+				arr[l] = arr[r];
+				arr[r] = temp;
+				l++;
+				r--;
+			}
+		}
+
+		if (left < l - 1)
+			quickSort(arr, left, l - 1);
+
+		if (l < right)
+			quickSort(arr, l, right);
 	}
 }
