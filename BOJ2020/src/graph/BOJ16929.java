41f7b953810b19396af72acf696993392f1e2b12
Hye-Yeon Choi <chlgpdus921@naver.com>
2020-05-26 19:25:19 +0900

    BOJ16929 Two Dots

---
+++ BOJ2020/src/graph/BOJ16929.java
@@ -0,0 +1,16 @@
+package graph;
+
+import java.io.IOException;
+/**
+ * 
+ * @FileName BOJ16929.java
+ * @author chlgpdus921
+ * @date 2020. 5. 26.
+ */
+/* Two Dots */
+public class BOJ16929 {
+	public static void main(String[] args) throws IOException {
+		
+		
+	}
+}
