e9f36e582c3898967a1427c82bcefa4a8175b6e1
Hye-Yeon Choi <chlgpdus921@naver.com>
2020-07-24 19:43:02 +0900

    BOJ14888 연산자 끼어넣기

--- BOJ2020/src/bruteforce_permutation/BOJ14888.java
+++ BOJ2020/src/bruteforce_permutation/BOJ14888.java
@@ -12,34 +12,84 @@ import java.util.*;
 /* 연산자 끼워넣기 */
 public class BOJ14888 {
 	static int n;
-	static int[] data, opeator;
+	static long[] data;
+	static String[] operator;
+	static long MaxValue = Long.MIN_VALUE;
+	static long MinValue = Long.MAX_VALUE;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
 		n = Integer.valueOf(br.readLine());
 
-		data = new int[n];
-		opeator = new int[4];
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int j = 0;
-			while (st.hasMoreElements()) {
+		data = new long[n];
+		operator = new String[n - 1];
 
+		String[] tempValue = { "+", "-", "*", "/" };
+
+		st = new StringTokenizer(br.readLine(), " ");
+		int j = 0;
+		while (st.hasMoreElements()) {
+			data[j] = Integer.valueOf(st.nextToken());
+			j++;
+		}
+
+		st = new StringTokenizer(br.readLine(), " ");
+		j = 0;
+		int c = 0;
+		while (st.hasMoreElements()) {
+			int index = Integer.valueOf(st.nextToken());
+			for (int i = 0; i < index; i++) {
+				operator[c] = tempValue[j];
+				c++;
 			}
+			j++;
 
 		}
 
+		for (int i = 0; i < n - 1; i++) {
+			boolean[] check = new boolean[n - 1];
+			check[i] = true;
+			long newValue = cal(data[0], 1, i);
+			dfs(check, 2, newValue);
+		}
+		System.out.println(MaxValue);
+		System.out.println(MinValue);
 	}
 
-	public static void dfs(boolean[] check, int index) {
-		if (index > n) {
+	public static void dfs(boolean[] check, int index, long beforesum) {
+
+		if (index > n - 1) {
+			if (beforesum > MaxValue)
+				MaxValue = beforesum;
+			if (MinValue > beforesum)
+				MinValue = beforesum;
+
 			return;
 		}
 
 		for (int i = 0; i < n - 1; i++) {
-			//+ - * / 
+			if (check[i] == false) {
+				check[i] = true;
+				long newValue = cal(beforesum, index, i);
+				dfs(check, index + 1, newValue);
+				check[i] = false;
+			}
 
 		}
+	}
+
+	public static long cal(long beforesum, int index, int i) {
+		if (operator[i].equals("+")) {
+			beforesum = (beforesum + data[index]);
+		} else if (operator[i].equals("-")) {
+			beforesum = (beforesum - data[index]);
+		} else if (operator[i].equals("*")) {
+			beforesum = (beforesum * data[index]);
+		} else if (operator[i].equals("/")) {
+			beforesum = (beforesum / data[index]);
+		}
+		return beforesum;
 	}
 }
