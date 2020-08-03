7eb98e74cc81a24f5440a62452c2fb0d9835b671
Hye-Yeon Choi <chlgpdus921@naver.com>
2020-08-04 05:25:52 +0900

    UPDATE BOJ2580 스도쿠

--- BOJ2020/src/bruteforce_recursion/BOJ2580.java
+++ BOJ2020/src/bruteforce_recursion/BOJ2580.java
@@ -12,107 +12,71 @@ import java.util.StringTokenizer;
 /* 스도쿠 */
 public class BOJ2580 {
 	static int[][] d;
-	static final int ROW = 0, COL = 1;
-	static final int[][] DISTANCE = { { 1, 0 }, { 2, 0 }, { 0, 1 }, { 0, 2 }, { 1, 1 }, { 1, 2 }, { 2, 1 }, { 2, 2 } };
+	static boolean[][] row, col, square;
+	static StringBuilder sb;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		d = new int[10][10];
+		sb = new StringBuilder();
+		d = new int[9][9];
+		row = new boolean[9][10];
+		col = new boolean[9][10];
+		square = new boolean[9][10];
 
 		for (int i = 0; i < 9; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			int j = 0;
 			while (st.hasMoreElements()) {
 				d[i][j] = Integer.valueOf(st.nextToken());
-				
+				if (d[i][j] != 0) {
+					row[i][d[i][j]] = true;
+					col[j][d[i][j]] = true;
+					square[checkSqaureIndex(i, j)][d[i][j]] = true;
+				}
 				j++;
 			}
 		}
-		bruteforce();
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				sb.append(d[i][j] + " ");
-			}
-			sb.append("\n");
-		}
-		System.out.println(sb);
+		dfs(0);
+
 	}
 
-	public static void bruteforce() {
-
-		for (int x = 1; x < 10; x++) {
-			for (int y = 1; y < 10; y++) {
-
-				int rowSum = 0;
-				int colSum = 0;
-				int squareSum = d[x][y];
-
-				int total1 = 0, total2 = 0, total3 = 0;
-				for (int i = 1; i < 10; i++) {
-					rowSum += d[x][i];
-					colSum += d[i][y];
-					if (d[x][i] == 0)
-						total1++;
-					if (d[i][y] == 0)
-						total2++;
+	public static void dfs(int cnt) {
+		if (cnt >= 81) {
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					sb.append(d[i][j] + " ");
 				}
-				if (x == 1 || x == 4 || x == 7) {
-					if (y == 1 || y == 4 || y == 7) {
+				sb.append("\n");
+			}
+			System.out.println(sb);
+			System.exit(0);
+		}
+		int x = cnt / 9;
+		int y = cnt % 9;
 
-						for (final int[] distance : DISTANCE) {
-							int newRow = x + distance[ROW];
-							int newCol = y + distance[COL];
-							if (d[newRow][newCol] == 0)
-								total3++;
-							squareSum += d[newRow][newCol];
+		if (d[x][y] != 0) {
+			dfs(cnt + 1);
+		} else {
+			for (int i = 1; i <= 9; i++) {
+				if (!row[x][i] && !col[y][i] && !square[checkSqaureIndex(x, y)][i]) {
+					d[x][y] = i;
+					row[x][i] = true;
+					col[y][i] = true;
+					square[checkSqaureIndex(x, y)][i] = true;
+					dfs(cnt + 1);
+					d[x][y] = 0;
+					row[x][i] = false;
+					col[y][i] = false;
+					square[checkSqaureIndex(x, y)][i] = false;
 
-						}
-					}
-
-				}
-
-				int value1 = 45 - rowSum;
-				int value2 = 45 - colSum;
-				int value3 = 45 - squareSum;
-
-				if (total1 == 1 && value1 > 0 && value1 < 10) {
-					fillNum(x, value1);
-				}
-				if (total2 == 1 && value2 > 0 && value2 < 10) {
-					fillNum2(y, value2);
-				}
-				if (total3 == 1 && value3 > 0 && value3 < 10) {
-					fillNum3(x, y, value3);
 				}
 			}
 		}
 
 	}
 
-	public static void fillNum(int x, int value) {
-		for (int j = 1; j < 10; j++) {
-			if (d[x][j] == 0)
-				d[x][j] = value;
-		}
-	}
-
-	public static void fillNum2(int x, int value) {
-		for (int j = 1; j < 10; j++) {
-			if (d[j][x] == 0)
-				d[j][x] = value;
-		}
-	}
-
-	public static void fillNum3(int x, int y, int value) {
-		for (final int[] distance : DISTANCE) {
-			int newRow = x + distance[ROW];
-			int newCol = y + distance[COL];
-			if (d[newRow][newCol] == 0) {
-				d[newRow][newCol] = value;
-			}
-
-		}
+	public static int checkSqaureIndex(int i, int j) {
+		return i / 3 * 3 + j / 3;
 	}
 
 }
