8a5efe9cd20f33392909bafb724d5fc7a0c65308
Hye-Yeon Choi <chlgpdus921@naver.com>
2021-01-05 20:42:09 +0900

    temp commit

---
+++ BOJ2020/src/myPackage/BOJ13335.java
@@ -0,0 +1,35 @@
+package myPackage;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ * @FileName BOJ13335.java
+ * @author chlgpdus921
+ * @date 2020. 12. 5.
+ */
+/* 트럭 */
+public class BOJ13335 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int n = Integer.valueOf(st.nextToken());
+		int w = Integer.valueOf(st.nextToken());
+		int l = Integer.valueOf(st.nextToken());
+
+		st = new StringTokenizer(br.readLine(), " ");
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.valueOf(st.nextToken());
+		}
+		
+		
+		
+		
+	}
+	
+
+}
